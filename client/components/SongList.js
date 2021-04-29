986de3932e85d6f476eaee7e61d2b29e816fb6bd
ninuannvarghese <ninuann86@gmail.com>
2021-04-29 18:24:58 -0400

    Added error handling for the client side

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,6 +1,19 @@
 import React from "react";
+import { GET_SONG } from "./queries/apolloQueries";
+import { useQuery } from "@apollo/client";
 
-function SongList() {
-  return <div>SongList</div>;
-}
+const SongList = () => {
+  const { data, loading, error } = useQuery(GET_SONG);
+  // console.log(data);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>ERROR: {error.message}</p>;
+  if (!data) return <p>Not found</p>;
+  return (
+    <>
+      {data && data.songs && data.songs.map((item) => <div>{item.title}</div>)}
+    </>
+  );
+};
+
 export default SongList;
--- client/index.js
+++ client/index.js
@@ -1,20 +1,57 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ApolloClient, ApolloProvider } from "@apollo/client";
-import { InMemoryCache } from "apollo-cache-inmemory";
-import { createHttpLink } from "apollo-link-http";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  HttpLink,
+  from,
+} from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
 import SongList from "./components/SongList";
 
-const cache = new InMemoryCache();
+//const cache = new InMemoryCache();
 
-const httpLink = createHttpLink({
-  uri: "http://localhost:4000/graphql",
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+
+  if (networkError) console.log(`[Network error]: ${networkError}`);
 });
+
+const link = from([
+  errorLink,
+  new HttpLink({ uri: `http://localhost:4000/graphql` }),
+]);
 
 const client = new ApolloClient({
-  link: httpLink,
-  cache,
+  link,
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          songs: {
+            read: (existing, { toReference, args }) => {
+              const SongRef = toReference({
+                __typename: "SongType",
+              });
+              return existing ?? SongRef;
+            },
+          },
+        },
+      },
+    },
+  }),
 });
+
+/*const client = new ApolloClient({
+  link,
+  cache,
+});*/
 
 const Root = () => {
   return (
