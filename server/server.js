00a9d24325c946800b08b658be1732a1e12ec5b4
Ninu Varghese <ninuann86@gmail.com>
2021-05-03 12:01:42 -0400

    Update server.js
    updated the mongo url

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Replace with your mongoLab URI
 const MONGO_URI =
-  "mongodb+srv://admin:admin@cluster0.shsvh.mongodb.net/rgql?retryWrites=true&w=majority";
+  "<MONGO_URL>";
 
 if (!MONGO_URI) {
   throw new Error("You must provide a MongoLab URI");
